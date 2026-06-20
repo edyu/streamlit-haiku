c8edcaf9b95377db9533acb7e52bfe5d3b3654fa
Ed Yu <edyu@users.noreply.github.com>
2026-06-19 18:19:46 -0700

    Add login/signup flow

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 import streamlit as st
-from backend.database import Database
 
-st.title('Haiku Haven')
+from backend.hub import Hub
+from frontend.pages import pages
 
-database = Database()
-query_results = database.execute_query('SELECT * FROM haikus')
-st.write(query_results)
+if 'hub' not in st.session_state:
+    st.session_state.hub = Hub()
+
+if 'logged_in' in st.session_state and st.session_state.logged_in:
+    page = st.navigation([pages['home'], pages['logout']])
+else:
+    page = st.navigation([pages['login'], pages['signup']])
+
+page.run()
+
+# st.title('Haiku Haven')
+
+# database = Database()
+# query_results = database.execute_query('SELECT * FROM haikus')
+# st.write(query_results)
